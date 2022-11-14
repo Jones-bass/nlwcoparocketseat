7e61e3cd7bc746adcf8e243af5d49d46a52b7a1d
jones-bass <jonesbass.tb@gmail.com>
2022-11-14 11:57:07 -0300

    Estruturando Seed

---
+++ server/prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Jones de Tobias Barreto',
+      email: 'tobiasbarreto@gmail.com',
+      avatarUrl: 'https://github.com/jones-bass.png',
+    }
+  });
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: 'Example Pool',
+      code: 'BOL123',
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        }
+      }
+    }
+  });
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-13T12:45:04.503Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR',
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-14T12:45:04.503Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              }
+            }
+          }
+        }
+      }
+    },
+  })
+}
+
+main()
