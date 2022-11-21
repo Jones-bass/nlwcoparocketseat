b7d3e8865ec140b65821f2bfe298661e34d81581
jones-bass <jonesbass.tb@gmail.com>
2022-11-21 12:03:57 -0300

    Page New e Pool

--- Mobille/src/screens/Pools.tsx
+++ Mobille/src/screens/Pools.tsx
@@ -1,14 +1,43 @@
-import { VStack, Icon} from 'native-base';
+import { useCallback, useState } from 'react';
+import { VStack, Icon, useToast, FlatList} from 'native-base';
 import { Octicons } from '@expo/vector-icons';
 
 import { Button } from '../components/Button';
 import { Header } from '../components/Header';
 
-import { useNavigation } from '@react-navigation/native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import { api } from '../services/api';
+import { Loading } from '../components/Loading';
+import { PoolCard, PoolCardPros } from '../components/PoolCard';
 
 
 export function Pools() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [pools, setPools] = useState<PoolCardPros[]>([])
+
   const { navigate } = useNavigation();
+  const toast = useToast();
+
+  async function fetchPools() {
+    try {
+      setIsLoading(true)
+      const response = await api.get('/pools');
+      setPools(response.data.pools);
+    } catch (error) {
+      console.log(error);
+      toast.show({
+        title: 'Não foi possível carregar os bolões',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchPools();
+  },[]))
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -21,6 +50,22 @@ export function Pools() {
           onPress={() => navigate('find')}
         />
       </VStack>
+      {
+        isLoading ? <Loading /> :
+          <FlatList 
+            data={pools}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <PoolCard 
+                data={item}
+                onPress={() => navigate('details', { id: item.id })}
+              />
+            )}
+            showsVerticalScrollIndicator={false}
+            _contentContainerStyle={{ pb: 10 }}
+            px={5}
+          />
+      }
     </VStack>
   );
 }
