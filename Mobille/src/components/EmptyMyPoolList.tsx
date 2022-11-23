2c2fe14f097ac56cde2bfd5b6c009ca26a9f64f9
jones-bass <jonesbass.tb@gmail.com>
2022-11-22 23:17:57 -0300

    Page Details e Components

--- Mobille/src/components/EmptyMyPoolList.tsx
+++ Mobille/src/components/EmptyMyPoolList.tsx
@@ -1,32 +1,34 @@
-import { useNavigation } from '@react-navigation/native';
 import { Row, Text, Pressable } from 'native-base';
 
-export function EmptyMyPoolList() {
-  const { navigate } = useNavigation()
+interface Props {
+  code: string;
+}
 
+export function EmptyMyPoolList({ code }: Props) {
   return (
     <Row flexWrap="wrap" justifyContent="center" p={4}>
       <Text color="gray.200" fontSize="sm" textAlign="center">
         Você ainda não está participando de {'\n'} nenhum bolão, que tal
       </Text>
 
-      <Pressable onPress={() => navigate('find')}>
+      <Pressable onPress={() => { }}>
         <Text textDecorationLine="underline" color="yellow.500" textDecoration="underline">
           buscar um por código
         </Text>
       </Pressable>
 
       <Text color="gray.200" fontSize="sm" mx={1}>
-        ou 
+        ou
       </Text>
 
-      <Pressable onPress={() => navigate('new')}>
+      <Pressable onPress={() => { }}>
         <Text textDecorationLine="underline" color="yellow.500" textDecoration="underline">
           criar um novo
         </Text>
       </Pressable>
 
       <Text color="gray.200" fontSize="sm" textAlign="center" fontFamily="heading">
+        {code}
       </Text>
     </Row>
   );
